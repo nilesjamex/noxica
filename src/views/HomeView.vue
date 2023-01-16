3e2eea300cf3255c2ad863c9b8beddaf1755de56
Niles <egbaidomehjamez@gmail.com>
2023-01-16 23:11:04 +0100

    feat: imported homepage component to page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="home">
-    <p>hi</p>
-  </div>
+  <Hero />
+  <Use />
+  <Hello />
+  <Benefits />
+  <Testimonials />
+  <Footer />
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Hello from "@/components/Hello.vue";
+import Hero from "@/components/Hero.vue";
+import Benefits from "@/components/Benefits.vue";
+import Use from "@/components/Use.vue";
+import Testimonials from "@/components/Testimonials.vue";
+import Footer from "@/components/Footer.vue";
+</script>
